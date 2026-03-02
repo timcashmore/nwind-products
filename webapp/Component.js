e4795b55701401de178da95b36dcd456da89d936
Tim Cashmore <tim.cashmore@sap.com>
2026-03-02 20:43:09 +0000

    Initial Commit @3/2/2026, 8:43:09 PM

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("productsnwind.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
